1d82e7cb02409a1ba60c4d6d8eb8702ddaf92e6a
Scot Morrison <scot.e.morrison@gmail.com>
2023-02-17 12:21:54 +0000

    Add initial project files and template code.

---
+++ ButlerBot/ButlerBot/Program.cs
@@ -0,0 +1,15 @@
+﻿using LeagueCustomMatchmaking.Bot_Core;
+
+namespace ButlerBot;
+
+internal class Program
+{
+    static Task Main(string[] args) => new Program().MainAsync();
+
+    public async Task MainAsync()
+    {
+        BotCore bot = new();
+
+        await bot.LoginAsync();
+    }
+}
